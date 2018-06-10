7e055771dd3a370106d67a883c1b006d1b700c63
Andrzej Kapolka <drzej.k@gmail.com>
2018-06-09 22:32:34 -0700

    Forgot to add these to the last commit.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = function(grunt) {
 
   require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
@@ -6,17 +8,49 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     babel: {
-      options: {
-        sourceMap: true
-      },
       dist: {
-        files: {
-        }
+        options: {
+          presets: ['env', 'react'],
+        },
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: '**/*.js',
+          dest: 'build/',
+        }],
       }
-    }
+    },
+    browserify: {
+      dist: {
+        options: {
+           transform: ['browserify-shim', 'uglifyify'],
+        },        
+        files: [{
+          expand: true,
+          cwd: 'build/client',
+          src: 'exercises/*.js',
+          dest: 'dist/',
+          ext: '.min.js',
+        }],
+      }
+    },
+    replace: function() {
+      var config = {};
+      for (var name of grunt.file.expand({cwd: 'src/client/exercises'}, ['*.js'])) {
+        var basename = path.basename(name, '.js');
+        config[basename] = {
+          options: {
+            patterns: [{match: 'path', replacement: `${basename}.min.js`}],
+          },
+          src: 'src/client/exercises/template.html',
+          dest: `dist/exercises/${basename}.html`,
+        };
+      }
+      return config;
+    }(),
   });
 
   // Default task(s).
-  grunt.registerTask('default', ['babel']);
+  grunt.registerTask('default', ['babel', 'browserify', 'replace']);
 
 };
